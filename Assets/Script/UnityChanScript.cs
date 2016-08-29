2a395ec0cf277ac9be9f864683938383b4d8617a
maro <utauyomiracle310@gmail.com>
2016-08-30 04:33:12 +0900

    Unity-Chan追加, 歩きモーション追加

---
+++ Assets/Script/UnityChanScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityChanScript : MonoBehaviour {
+    private Animator anim;
+
+    void Start () {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey ("up")) {
+            anim.SetBool ("Walk", true);
+        } else {
+            anim.SetBool ("Walk", false);
+        }
+    }
+}
